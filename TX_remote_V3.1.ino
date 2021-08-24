8e49a64ee39fb8bb9261171334bbf7c8e3d2c9b9
lizard-brain <37095041+lizard-brain@users.noreply.github.com>
2021-08-24 19:42:11 +1000

    Add files via upload

---
+++ TX_remote_V3.1.ino
@@ -0,0 +1,66 @@
+#define HWSERIAL Serial1
+#include <MIDI.h>
+int Return = 0;
+int type, note, velocity, channel, d1, d2, key;
+
+int remote = 0;
+
+/* = 
+ *  0 =  turn all stacks off
+ *  1 = turn all stacks white
+ *  2 = all stacks red
+ *  3 = all stacks green
+ *  4 = all stacks blue
+
+*/
+
+void setup() {
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(31250);
+  
+  pinMode(13, OUTPUT);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  
+  Serial.println("MIDI Input Test");
+  
+  
+}
+
+unsigned long t=0;
+
+void loop() {
+
+for(int i = 12; i < 101; i++){
+      
+        type = 1;
+        note = i;
+        velocity = 126;
+        channel = 0;
+
+        Serial.println(String("~") + type + "," + channel + "," + velocity + "," + note + "~");
+        
+        Serial1.println(String("~") + type + "," + channel + "," + velocity + "," + note + "~");
+        delay(250);
+}
+
+delay(1000);
+
+for(int i = 12; i < 101; i++){
+      
+        type = 0;
+        note = i;
+        velocity = 0;
+        channel = 0;
+
+        Serial.println(String("~") + type + "," + channel + "," + velocity + "," + note + "~");
+        
+        Serial1.println(String("~") + type + "," + channel + "," + velocity + "," + note + "~");
+        delay(50);
+}
+
+delay(3000);
+
+       
+}
---
+++ TX_v3.0.ino
@@ -0,0 +1,37 @@
+#define HWSERIAL Serial1
+#include <MIDI.h>
+int Return = 0;
+int type, note, velocity, channel, d1, d2;
+
+void setup() {
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(9600);
+  HWSERIAL.begin(31250);
+  
+  pinMode(13, OUTPUT);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  
+  Serial.println("MIDI Input Test");
+  
+  
+}
+
+unsigned long t=0;
+
+void loop() {
+  
+  if (MIDI.read()) {                    // Is there a MIDI message incoming ?
+
+        type = MIDI.getType();
+        note = MIDI.getData1();
+        velocity = MIDI.getData2();
+        channel = MIDI.getChannel();
+
+        Serial.println(String("~") + type + "," + channel + "," + velocity + "," + note + "~");
+        
+        HWSERIAL.println(String("~") + type + "," + channel + "," + velocity + "," + note + "~");
+                  
+       }
+}
